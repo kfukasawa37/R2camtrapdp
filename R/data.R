dee1939f72edd303aa1fbd266d4aef1f3bddba98
KanaTerayama <kana@u-ga.net>
2026-02-06 19:13:03 +0900

    data update

--- R/data.R
+++ R/data.R
@@ -20,3 +20,16 @@ NULL
 #' @references Originally data can be used doi.xxx.xxx
 #' @keywords data camtrapDP
 NULL
+
+#' Deployment data of images which collected by 10 camera-trap (dummy data)
+#' @name Idep
+#' @docType data
+#' @keywords data
+#' 
+NULL
+
+#' Observation data of image and media-based observation which collected by 10 camera-trap (dummy data)
+#' @name Iobs
+#' @docType data
+#' @keywords data
+NULL
